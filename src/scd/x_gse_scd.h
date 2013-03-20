b39b254bf36a5b1ac54902d99aedacf5935cdddc
root <root@jorhy-ThinkPad-X61.(none)>
2013-03-20 17:38:56 +0800

    new file:   codelite/joies/goose.project 	new file:   codelite/joies/jo_pcap.project 	new file:   codelite/joies/jo_scd.project 	new file:   codelite/joies/joes.workspace 	new file:   codelite/joies/joes.workspace.session 	new file:   codelite/joies/joies.project 	new file:   codelite/joies/joies.tags 	new file:   src/pcap/jo_pcap.cpp 	new file:   src/pcap/jo_pcap.h 	new file:   src/scd/x_gse_scd.cpp 	new file:   src/scd/x_gse_scd.h 	new file:   src/scd/x_scd.cpp 	new file:   src/scd/x_scd.h 	new file:   src/scd/x_scd_def.h

---
+++ src/scd/x_gse_scd.h
@@ -0,0 +1,48 @@
+#ifndef __X_GSE_SCD_H_
+#define __X_GSE_SCD_H_
+
+#include "tinyxml.h"
+#include "json.h"
+#include "x_scd_def.h"
+
+struct GSE_DataInfo
+{
+    std::string desc;
+    std::string data_type;
+    std::string fc;
+};
+typedef std::vector<GSE_DataInfo *> GSE_AllDataVec;
+struct GSE_DataSetInfo
+{
+    std::string desc;
+    GSE_AllDataVec all_data;
+};
+typedef std::map<u_int32_t, GSE_DataSetInfo *> GSE_GocbMap; //key为appid
+
+class CXGseScd
+{
+    public:
+        CXGseScd();
+        ~CXGseScd();
+
+    public:
+        int Init();
+        void Deinit();
+        int GetAllCtrlBlock();
+
+    private:
+        int ParseIEDs(TiXmlNode *pRootNode);
+        int FindAppid(const std::string &ied_name, const std::string &ld_inst, const std::string &gocb,  u_int32_t &appid);
+        int MakeDataSetInfo(TiXmlNode *pLN0Node, const std::string &ds_name, GSE_DataSetInfo *pDsInfo);
+        int FindLNType(TiXmlNode *pLN0Node, const std::string &prefix, const std::string &ln_class, const std::string &ln_inst, const std::string &do_name, std::string &ds_desc, std::string &ln_type);
+        int FindDAType(const std::string &ln_type, const std::string &do_name, const std::string &da_name, std::string &data_type);
+        int FindType(const std::string &do_type, const std::string &da_name, std::string &data_type);
+
+    private:
+        TiXmlDocument m_xmlDoc;
+        TiXmlNode *m_pCommNode;
+        TiXmlNode *m_pDatTypeNode;
+
+        GSE_GocbMap m_gocbMap;
+};
+#endif // __X_GSE_SCD_H_
