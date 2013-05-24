9a73e48b2b31a0e3adb1d6c2214ae68fbfa49ad2
joorhy <joorhy@gmail.com>
2013-05-24 17:20:51 +0800

    new file:   src/misc/x_inet.cpp

---
+++ src/misc/x_inet.cpp
@@ -0,0 +1,12 @@
+#include "j_common.h"
+
+j_int64_t ntohll(j_int64_t val)
+{
+	return (((j_int64_t)htonl((j_int32_t)((val << 32) >> 32))) << 32) | (j_int32_t)htonl((j_int32_t)(val >> 32));
+}
+
+j_int64_t htonll(j_int64_t val)
+
+{
+	return (((j_int64_t)htonl((j_int32_t)((val << 32) >> 32))) << 32) | (j_int32_t)htonl((j_int32_t)(val >> 32));
+}
